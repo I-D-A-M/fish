e785d72d5817d9c6821cc4e71a992a6f2bbb49e3
Innes Morrison <innesdmorrison@gmail.com>
2016-02-16 15:32:53 +0000

    Removed un-used function git_things_to_commit.fish

--- functions/git_things_to_commit.fish
+++
@@ -1,6 +0,0 @@
-function git_things_to_commit -d "check to see how many changes are waiting to be commited to a git repo."
-	if not git_is_repo
-return 1
-end
-command git status --porcelain | wc -l
-end
